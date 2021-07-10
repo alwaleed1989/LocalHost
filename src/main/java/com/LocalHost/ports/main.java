1aa25bc49d7459d31937bcb8e5d05be7e2047a6e
alwaleed <waleee1eeed@gmail.com>
2021-07-10 04:28:32 +0300

    update

--- src/main/java/com/LocalHost/ports/main.java
+++ src/main/java/com/LocalHost/ports/main.java
@@ -4,6 +4,13 @@ public class main {
     static CheckPort check = new CheckPort();
     public static void main(String[] args) throws Exception {
 
+        System.out.println("    __                     ____  __           __     ____             __ \n" +
+                "   / /   ____  _________ _/ / / / /___  _____/ /_   / __ \\____  _____/ /_\n" +
+                "  / /   / __ \\/ ___/ __ `/ / /_/ / __ \\/ ___/ __/  / /_/ / __ \\/ ___/ __/\n" +
+                " / /___/ /_/ / /__/ /_/ / / __  / /_/ (__  ) /_   / ____/ /_/ / /  / /_  \n" +
+                "/_____/\\____/\\___/\\__,_/_/_/ /_/\\____/____/\\__/  /_/    \\____/_/   \\__/  \n" +
+                "                                                                         \n");
+        System.out.println("Checking The Port...");
         check.Check();
 
     }
